e23cff4bef3924ffd8007badb98d60c3b22583c2
Alex Gomozov <letonik3@gmail.com>
2022-09-23 15:21:20 +0300

    L_2.4

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,6 +2,12 @@ import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
 
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: ["style-loader", "css-loader", "sass-loader"],
+    exclude: /node_modules/,
+  }
+
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
@@ -10,5 +16,6 @@ export function buildLoaders(): webpack.RuleSetRule[] {
 
   return  [
     typescriptLoader,
+    cssLoader
   ]
 }
---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss'
+
+const App = () => {
+  return (
+    <div>
+      <button>ddddddddddddddddd</button>
+    </div>
+  );
+};
+
+export default App;
