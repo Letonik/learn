a7383421223dc46d32d9f85a4568fa4cbc858393
Alex Gomozov <letonik3@gmail.com>
2022-09-30 17:08:38 +0300

    L_2.13

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
-  const {theme, toggleTheme} = useTheme()
+  const {theme} = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar/>
       <AppRouter/>
-      <button onClick={toggleTheme}>Theme</button>
     </div>
   );
 };
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss'
-import {AppLink, AppLinkTheme} from "shared/ui/AppLink";
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 
 interface NavbarProps {
   className?: string,
 }
 
 export const Navbar = ({className}: NavbarProps) => {
-  console.log('cls')
-  console.log(cls)
+
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
+      <ThemeSwitcher />
       <div className={cls.links}>
         <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>Главная</AppLink>
         <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>О сайте</AppLink>
@@ -19,4 +20,4 @@ export const Navbar = ({className}: NavbarProps) => {
     </div>
   );
 };
-;
+
