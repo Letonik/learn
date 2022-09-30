faf7b1c187f619afb6f9ea7ef39aefa55fe4b998
Alex Gomozov <letonik3@gmail.com>
2022-09-30 18:02:55 +0300

    L_2.14

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -9,7 +9,15 @@ const AppRouter = () => {
     <Suspense fallback={<div>Load...........</div>}>
       <Routes>
         {Object.values(routeConfig).map(({element, path}) => (
-          <Route key={path} path={path} element={element}/>
+          <Route
+            key={path}
+            path={path}
+            element={
+              <div className={'page-wrapper'}>
+                {element}
+              </div>
+            }
+          />
         ))}
       </Routes>
     </Suspense>
