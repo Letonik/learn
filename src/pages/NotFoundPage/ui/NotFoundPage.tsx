c2cecd7c4293ebbc3bbc1ddb15c8e7f43b8bc2e6
Alex Gomozov <letonik3@gmail.com>
2022-10-05 19:10:29 +0300

    L_2.21

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './NotFoundPage.module.scss';
+
+interface NotFoundPageProps {
+  className?: string,
+}
+
+export const NotFoundPage = ({ className }: NotFoundPageProps) => {
+  return (
+    <div className={classNames(cls.NotFoundPage, {}, [className])}>
+
+    </div>
+  );
+};
