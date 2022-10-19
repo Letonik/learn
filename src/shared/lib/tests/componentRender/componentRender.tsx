74ccb5dacd9e567eaacc31990e952bd5521550fc
Alex Gomozov <letonik3@gmail.com>
2022-10-19 19:45:08 +0300

    l_3.28

---
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react';
+import { render } from '@testing-library/react';
+import { I18nextProvider } from 'react-i18next';
+import { MemoryRouter } from 'react-router-dom';
+import i18nForTest from 'shared/config/i18n/i18nForTest';
+
+export interface componentRenderOptions {
+  route?: string;
+}
+
+export function componentRender(component: ReactNode, options: componentRenderOptions = {}) {
+
+  const {
+    route = '/',
+  } = options;
+
+  return render(
+    <MemoryRouter initialEntries={[route]}>
+      <I18nextProvider i18n={i18nForTest}>
+        {component}
+      </I18nextProvider>
+    </MemoryRouter>,
+  );
+}
